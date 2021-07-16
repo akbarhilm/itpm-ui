2b52397896dcdac428c0094190331ed538dde859
M Mustakim <museboy90@gmail.com>
2021-07-16 07:04:36 +0700

    fix login

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,11 +1,12 @@
 import Cookies from "universal-cookie";
+import Crypto from 'crypto-js';
 
 const cookies = new Cookies();
 
 export function getAuth() {
   const auth = cookies.get('auth');
   if (auth) {
-    return "Bearer " + auth;
+    return "Bearer " + Crypto.AES.decrypt(auth, "encrypt-token-for-cookie");
   } else {
     return "";
   }
