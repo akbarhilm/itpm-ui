6091ee7e1c9fd57bde880d04193d7fac31cdf2d9
mustakim <mustakim@indonesian-aerospace.com>
2021-07-28 09:23:43 +0700

    fixed auth

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -5,10 +5,8 @@ const cookies = new Cookies();
 
 export function getAuth() {
   const auth = cookies.get('auth');
-  const originalToken = Crypto.AES.decrypt(auth, "encrypt-token-for-cookie").toString(Crypto.enc.Utf8);
   if (auth) {
-    // console.log("coo", auth);
-    // console.log("de", originalToken);
+    const originalToken = Crypto.AES.decrypt(auth, "encrypt-token-for-cookie").toString(Crypto.enc.Utf8);
     return "Bearer " + originalToken;
   } else {
     return "";
